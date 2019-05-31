b4e81a4a74113817064683726d7b1d52ccf4bf3c
Mark Hernandez <m.hern1415@gmail.com>
2019-05-31 20:24:54 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -2,22 +2,29 @@
 
 class Genre
 
-   attr_reader :name, :genre
-  attr_accessor :song
+attr_reader :name, :genre
+attr_accessor :song
 
-   def initialize(name)
-    @name = name
-  end
+@@all = []
 
-   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
-  end
+def initialize(name)
+  @name = name
+end
 
-   def artists
-    Song.all.map do |song|
-      song.artist
-    end
+def songs
+  Song.all.select do |song|
+    song.genre == self
   end
+end
+
+def artists
+  Song.all.map do |song|
+    song.artist
+  end
+end
+
+def self.all
+  @@all
+end
+
 end
