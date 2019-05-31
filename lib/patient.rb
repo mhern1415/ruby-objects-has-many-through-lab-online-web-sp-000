69b3aadcf7851630b1e9483f7a742adcd5e312e4
Mark Hernandez <m.hern1415@gmail.com>
2019-05-31 21:03:30 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,29 +1,29 @@
 class Patient
 
-   attr_reader :name, :appointment, :doctor
+attr_accessor :name, :appointment, :doctor
 @@all = []
-   def initialize(name)
-    @name = name
-    @@all << self
-  end
+def initialize(name)
+  @name = name
+  @@all << self
+end
 
-   def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
-  end
+def new_appointment(doctor, date)
+  Appointment.new(self, doctor, date)
+end
 
-   def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
+def appointments
+  Appointment.all.select do |appointment|
+    appointment.patient == self
   end
+end
 
-   def doctors
-    Appointment.all.map do |appointment|
-      appointment.doctor
-    end
+def doctors
+  Appointment.all.map do |appointment|
+    appointment.doctor
   end
+end
   
-  def self.all
-    @@all
-  end
+def self.all
+  @@all
+end
 end
