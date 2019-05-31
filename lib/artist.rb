a638bb94ccc5370f67d5099d464ec4760632df43
Mark Hernandez <m.hern1415@gmail.com>
2019-05-31 20:33:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,9 @@ def new_song(name, genre)
 end
 
 def new_song(name, genre)
-    song = Song.new(name, self, genre)
-    self.songs.last
+  song = Song.new(name, self, genre)
+  self.songs.last
+end
 
   
   
